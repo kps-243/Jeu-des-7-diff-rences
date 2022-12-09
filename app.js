f86eac3fd3df6f9b8dc2fbf8488ce199a1a806d8
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-09 14:12:38 +0100

    Séparation des 2 images

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ let ctx1 = canvas1.getContext("2d");
 let ctx2 = canvas2.getContext("2d");
 
 // Dessin de l'image de départ sur le premier canvas
-new Image.src () = 
+let img1 = new Image();
+img1.src = "/img/diff1.jpg";
+
+let img2 = new Image();
+img2.src = "/img/diff2.jpg";
+
+img1.onload = function () {
+  ctx1.drawImage(img1, 10, 0);
+};
+img2.onload = function () {
+  ctx1.drawImage(img2, 500, 0);
+};
 
 // Dessin de l'image finale sur le deuxième canvas
-ctx2.fillStyle = "red";
-ctx2.fillRect(10, 10, 30, 30);
-ctx2.fillStyle = "green";
-ctx2.fillRect(60, 10, 30, 30);
+
+// ctx2.fillStyle = "red";
+// ctx2.fillRect(10, 10, 30, 30);
+// ctx2.fillStyle = "green";
+// ctx2.fillRect(60, 10, 30, 30);
 
 // Fonction pour détecter les différences entre les deux images
 function findDifferences() {
