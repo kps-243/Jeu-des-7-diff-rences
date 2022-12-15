b5a6b7764b8a5d791839d7a5c68d803c6127eb97
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-15 13:30:05 +0100

    Création de la fonction onclick

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-let area = document.querySelector("area");
-let canvas = document.getElementById("canvas1");
-let ctx = canvas.getContext("2d");
+let img = document.querySelector("img");
+let cercle1 = document.querySelector(".cercle1");
 var actif = 0;
 var victoire = 0;
-btn1.addEventListener("click", () => {
-  actif = 1;
-  curseur.style.display = "block";
-  bordure = "red";
-});
 
-btn21.addEventListener("click", () => {
+function circleAppear() {
+  actif++;
   if (actif == 1) {
-    btn1.style.border = "0px";
-    btn21.style.border = "0px";
     victoire++;
-    curseur.style.display = "none";
-    btn1.style.transform = "scale(0.5)";
-    btn21.style.transform = "scale(0.5)";
+    cercle1.style.display = "block";
     actif = 0;
   }
-  if (victoire == 4) {
-    document.querySelector("#victoire").style.display = "block";
-  }
-});
+  console.log("bonjour");
+}
