2891d852c8d8988663310637328ed823743718b3
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-16 15:01:41 +0100

    Cercle apparition

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-let img = document.querySelector("img");
 let cercle1 = document.querySelector(".cercle1");
-var actif = 0;
-var victoire = 0;
+let cercle2 = document.querySelector(".cercle2");
+let cercle3 = document.querySelector(".cercle3");
+let cercle4 = document.querySelector(".cercle4");
+let cercle5 = document.querySelector(".cercle5");
 
-function circleAppear() {
-  actif++;
-  if (actif == 1) {
-    victoire++;
-    cercle1.style.display = "block";
-    actif = 0;
-  }
-  console.log("bonjour");
+function circleAppear(p_evt) {
+  console.log(p_evt);
+  p_evt.style.opacity = "100%";
 }
