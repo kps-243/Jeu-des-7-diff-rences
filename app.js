f5215a1339f662919dd01a83529795f934406afc
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-19 22:33:45 +0100

    UPD Placement du cercle 1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let cercle2 = document.querySelector(".cercle2");
 let cercle3 = document.querySelector(".cercle3");
 let cercle4 = document.querySelector(".cercle4");
 let cercle5 = document.querySelector(".cercle5");
+let img2 = document.querySelector(".m-img");
 
 function circleAppear(p_evt) {
   console.log(p_evt);
