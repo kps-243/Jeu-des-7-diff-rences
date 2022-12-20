133c58b80761e747738f4401e485f685d508b0e4
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-20 22:48:13 +0100

    Essai avec des structures conditionnelles

--- app.js
+++ app.js
@@ -9,27 +9,26 @@ let cercle32 = document.querySelector(".cercle3-2");
 let cercle42 = document.querySelector(".cercle4-2");
 let cercle52 = document.querySelector(".cercle5-2");
 
-function circleAppear(p_evt) {
-  console.log(p_evt);
-  p_evt.style.opacity = "100%";
+function circleAppear() {
+  if (cercle1 || cercle12) {
+    cercle1.style.opacity = "100%";
+    cercle12.style.opacity = "100%";
+  }
+  if (cercle2 || cercle22) {
+    cercle2.style.opacity = "100%";
+    cercle22.style.opacity = "100%";
+  }
 }
-if ((p_evt = cercle1 || cercle12)) {
-  cercle1.style.opacity = "100%";
-  cercle12.style.opacity = "100%";
-}
-if ((p_evt = cercle2 || cercle22)) {
-  cercle2.style.opacity = "100%";
-  cercle22.style.opacity = "100%";
-}
-if ((p_evt = cercle3 || cercle32)) {
-  cercle3.style.opacity = "100%";
-  cercle32.style.opacity = "100%";
-}
-if ((p_evt = cercle4 || cercle42)) {
-  cercle4.style.opacity = "100%";
-  cercle42.style.opacity = "100%";
-}
-if ((p_evt = cercle5 || cercle52)) {
-  cercle5.style.opacity = "100%";
-  cercle52.style.opacity = "100%";
-}
+
+// if ((p_evt = cercle3 || cercle32)) {
+//   cercle3.style.opacity = "100%";
+//   cercle32.style.opacity = "100%";
+// }
+// if ((p_evt = cercle4 || cercle42)) {
+//   cercle4.style.opacity = "100%";
+//   cercle42.style.opacity = "100%";
+// }
+// if ((p_evt = cercle5 || cercle52)) {
+//   cercle5.style.opacity = "100%";
+//   cercle52.style.opacity = "100%";
+// }
