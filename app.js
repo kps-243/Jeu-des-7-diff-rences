f6364c9f8a4840ceaeb0efa117fd51063e02a474
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-20 18:01:51 +0100

    UPD Ajout des cercles de images 2

--- app.js
+++ app.js
@@ -3,9 +3,33 @@ let cercle2 = document.querySelector(".cercle2");
 let cercle3 = document.querySelector(".cercle3");
 let cercle4 = document.querySelector(".cercle4");
 let cercle5 = document.querySelector(".cercle5");
-let img2 = document.querySelector(".m-img");
+let cercle12 = document.querySelector(".cercle1-2");
+let cercle22 = document.querySelector(".cercle2-2");
+let cercle32 = document.querySelector(".cercle3-2");
+let cercle42 = document.querySelector(".cercle4-2");
+let cercle52 = document.querySelector(".cercle5-2");
 
 function circleAppear(p_evt) {
   console.log(p_evt);
   p_evt.style.opacity = "100%";
+}
+if ((p_evt = cercle1 || cercle12)) {
+  cercle1.style.opacity = "100%";
+  cercle12.style.opacity = "100%";
+}
+if ((p_evt = cercle2 || cercle22)) {
+  cercle2.style.opacity = "100%";
+  cercle22.style.opacity = "100%";
+}
+if ((p_evt = cercle3 || cercle32)) {
+  cercle3.style.opacity = "100%";
+  cercle32.style.opacity = "100%";
+}
+if ((p_evt = cercle4 || cercle42)) {
+  cercle4.style.opacity = "100%";
+  cercle42.style.opacity = "100%";
+}
+if ((p_evt = cercle5 || cercle52)) {
+  cercle5.style.opacity = "100%";
+  cercle52.style.opacity = "100%";
 }
