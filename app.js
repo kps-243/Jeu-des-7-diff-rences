fba32bd452f13fe325154f518855842db06fbe68
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-03 13:41:07 +0100

    Essai avec des id

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-let cercle1 = document.querySelector(".cercle1");
-let cercle2 = document.querySelector(".cercle2");
-let cercle3 = document.querySelector(".cercle3");
-let cercle4 = document.querySelector(".cercle4");
-let cercle5 = document.querySelector(".cercle5");
-let cercle12 = document.querySelector(".cercle1-2");
-let cercle22 = document.querySelector(".cercle2-2");
-let cercle32 = document.querySelector(".cercle3-2");
-let cercle42 = document.querySelector(".cercle4-2");
-let cercle52 = document.querySelector(".cercle5-2");
+let cercle1 = document.querySelector("#cercle1");
+let cercle2 = document.querySelector("#cercle2");
+let cercle3 = document.querySelector("#cercle3");
+let cercle4 = document.querySelector("#cercle4");
+let cercle5 = document.querySelector("#cercle5");
+let cercle12 = document.querySelector("#cercle1-2");
+let cercle22 = document.querySelector("#cercle2-2");
+let cercle32 = document.querySelector("#cercle3-2");
+let cercle42 = document.querySelector("#cercle4-2");
+let cercle52 = document.querySelector("#cercle5-2");
 
-function circleAppear() {
-  if (cercle1 || cercle12) {
-    cercle1.style.opacity = "100%";
-    cercle12.style.opacity = "100%";
-  }
-  if (cercle2 || cercle22) {
-    cercle2.style.opacity = "100%";
-    cercle22.style.opacity = "100%";
-  }
+function circleAppear(cercle) {
+  console.log(cercle.style.opacity);
+  const cssobj = window.getComputedStyle(cercle, null);
+  let opacity = cssobj.getPropertyValue("opacity");
+  cercle.style.opacity = "1";
+  opacity = parseInt(opacity);
+  console.log(cercle.style.opacity);
 }
 
 // if ((p_evt = cercle3 || cercle32)) {
