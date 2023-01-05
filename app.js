66ac2e70410f8829637b81e64062aa81b20e0914
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-05 11:57:06 +0100

    UPD fin ajustement

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let cercle2 = document.querySelector(".cercle2");
 let cercle3 = document.querySelector(".cercle3");
 let cercle4 = document.querySelector(".cercle4");
 let cercle5 = document.querySelector(".cercle5");
+let cercle6 = document.querySelector(".cercle6");
+let cercle7 = document.querySelector(".cercle7");
 
 function circleAppear(cercle) {
   console.log("coucou");
@@ -11,7 +13,4 @@ function circleAppear(cercle) {
     newcercle.style.opacity = "1";
     console.log(newcercle.style.opacity);
   });
-  // console.log(cercle.style.opacity);
-  //
-  // console.log(newcercle);
 }
